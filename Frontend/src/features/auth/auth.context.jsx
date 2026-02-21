558726bbfe30056562cee6138a6333033c5cc68f
mehulj-cpp <mehulj1735@gmail.com>
2026-02-21 16:52:40 +0530

    Add Protected route component and user loading logic in authentication context

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -7,8 +7,9 @@ export const AuthContext = createContext()
 export const AuthProvider = ({ children }) => { 
 
     const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
+    
 
 
     return (
