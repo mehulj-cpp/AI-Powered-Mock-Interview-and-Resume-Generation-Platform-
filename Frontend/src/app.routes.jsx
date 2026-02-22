20aab6d56cce692f8cac66ae54badd4b7a56eded
mehulj-cpp <mehulj1735@gmail.com>
2026-02-22 19:26:23 +0530

    feat: add Home component and update routing; enhance user authentication loading logic and styles

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,19 +2,20 @@ import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
+import Home from "./features/interview/pages/Home";
 
 
 export const router = createBrowserRouter([
     {
         path: "/login",
-        element:<Login />
+        element: <Login />
     },
     {
         path: "/register",
-        element:<Register />
+        element: <Register />
     },
     {
-        path:"/",
-        element:<Protected><h1>Home page</h1></Protected>
+        path: "/",
+        element: <Protected><Home /></Protected>
     }
 ])
